54d642a94618e3926bfd0eba085385806c8d7272
Meriç Cana <mmericcana@gmail.com>
2024-09-06 16:48:52 +0300

    Added redirection to how card object

--- src/components/Hov-Card/HovCard.tsx
+++ src/components/Hov-Card/HovCard.tsx
@@ -17,7 +17,7 @@ export default function HovCard(props) {
     return (
         <HoverCard>
             <HoverCardTrigger asChild>
-                <Button variant="link" className="p-0">@{props.name}</Button>
+                <a href={"user/"+props.name}><Button variant="link" className="p-0">@{props.name}</Button></a>
             </HoverCardTrigger>
             <HoverCardContent className="w-80">
                 <div className="flex justify-between space-x-4">
