8d19826f60c98ab8418f077612557c13fef930be
Meriç Cana <mmericcana@gmail.com>
2025-02-27 21:48:10 +0300

    update environments render.com

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,7 @@
 export const Environment = {
     production: false,
-    baseUrl: "http://localhost:8080/",
-    //baseUrl: "https://whisper-core-production.up.railway.app/",
-    //domain: "https://xn--sylenti-90a.com/"
-    domain: "http://localhost:3000/"
+    //baseUrl: "http://localhost:8080/",
+    baseUrl: "https://whisper-core.onrender.com",
+    domain: "https://xn--sylenti-90a.com/"
+    //domain: "http://localhost:3000/"
   };
